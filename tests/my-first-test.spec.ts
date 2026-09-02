95f2d7829d7f1ea2a7042e34171c0687e014c02b
mythupc <mythupc@gmail.com>
2026-09-02 20:40:49 +0530

    added new file with valid & invalid login scenario

--- tests/my-first-test.spec.ts
+++ tests/my-first-test.spec.ts
@@ -2,6 +2,6 @@ import { test, expect } from "@playwright/test";
 
 test("verify Example Domain", async ({ page }) => {
   await page.goto("https://example.com");
-
+   await page.pause();
   await expect(page).toHaveTitle("Example Domain");
 });
